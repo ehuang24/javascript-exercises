010c526910b8157aa019465474962dd8354f5c31
Emily Huang <ehuang2@alumni.stanford.edu>
2026-04-27 20:32:20 -0400

    add solution to 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        let total = "";
+        for (let i = 0; i < num; i++ ) {
+            total += string
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
