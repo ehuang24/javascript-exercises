fec0909a18b0ad9f3ee7ebd505e9944e6ef84e1e
Emily Huang <ehuang2@alumni.stanford.edu>
2026-04-27 21:02:20 -0400

    add solution to 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    const res = array.filter((num) => {for (let i = 0; i < theArgs.length; i++) {if (num === theArgs[i]) {return false;}} return true;});
+    return res;
 };
 
 // Do not edit below this line
