97a9e1fb11f38d1c9ea42473585904961e92dc79
Emily Huang <ehuang2@alumni.stanford.edu>
2026-04-27 21:13:19 -0400

    add solution to 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) === false || Number.isInteger(b) === false || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a > b) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+    res = 0;
+    for (let i = a; i <= b; i++) {
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
