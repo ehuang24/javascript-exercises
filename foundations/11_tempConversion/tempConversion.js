c6025dce5df048815bf5267ed47221ab05296cf7
Emily Huang <ehuang2@alumni.stanford.edu>
2026-04-27 21:24:57 -0400

    add solution to 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  celsius = Math.round((5 / 9 * (temperature - 32)) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  fahrenheit = Math.round((9 / 5 * temperature + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
