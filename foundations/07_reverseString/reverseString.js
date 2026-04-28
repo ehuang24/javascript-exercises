8681cfbabc17414bcf941c7dc4a740db57478d93
Emily Huang <ehuang2@alumni.stanford.edu>
2026-04-27 20:39:44 -0400

    add solution to 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let res = "";
+    for (let i = 0; i < string.length; i++) {
+        res = string[i] + res
+    }
+    return res
 };
 
 // Do not edit below this line
